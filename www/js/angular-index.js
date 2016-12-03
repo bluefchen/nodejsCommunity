c696a50f0f9a67dd6a92229002cc200ed481ba22
maomao <635805060@qq.com>
2016-12-03 23:10:05 +0800

    首页界面初步搭建

---
+++ www/js/angular-index.js
@@ -0,0 +1,11 @@
+angular
+    .module('indexPage', [])
+    .controller('mainController', [function(){
+
+    }])
+    .controller('form-login-controller', [function(){
+
+    }])
+    .controller('form-register-controller', [function(){
+        
+    }])
