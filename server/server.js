7bc9673994adc3a46a2a63052a5ee6d0540bb017
maomao <635805060@qq.com>
2016-12-14 20:24:41 +0800

    处理404状态

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use( require('./routers/user/login') );
 app.use( require('./routers/question/question') );
 app.use( require('./routers/answer/answer') );
 app.use( require('./routers/topic/topic') );
-
+app.use( require('./routers/404') );
 
 
 
